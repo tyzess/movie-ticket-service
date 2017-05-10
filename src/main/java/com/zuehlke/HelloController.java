6789719d0bbbea0a9a27a2eced331d291036a524
Zysset <Urs.Zysset@zuehlke.com>
2017-05-10 09:29:22 +0200

    Spring Boot skeleton

---
+++ src/main/java/com/zuehlke/HelloController.java
@@ -0,0 +1,18 @@
+package com.zuehlke;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by urzy on 10.05.2017.
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String home() {
+        return "Hello World!";
+    }
+}
